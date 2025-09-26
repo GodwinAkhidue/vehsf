08a77ef07f25e67bac81e6312d8d215eb5006949
“[Godwin <179575272+GodwinAkhidue@users.noreply.github.com>
2025-09-26 09:28:56 +0100

    Update server_url.tsx

--- constants/server_url.tsx
+++ constants/server_url.tsx
@@ -1,2 +1,2 @@
-// export const server_url = "https://api.vehsf.com";
-export const server_url = "http://localhost:5000";
+export const server_url = "https://api.vehsf.com";
+// export const server_url = "http://localhost:5000";

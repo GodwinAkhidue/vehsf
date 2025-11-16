03df5e6f7ad2b9c0f432597a6d835e105e38936b
GodwinAkhidue <ebehiakhidue2099@gmail.com>
2025-11-16 20:14:29 +0100

    about page images changed

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -13,13 +13,13 @@ export default function About() {
                 </div>
                 <div className="flex flex-col lg:flex-row items-center justify-center w-full mt-[30px]">
                     <div className="w-full max-w-[350px] h-[225px] lg:-mr-[20px] -mb-[20px] lg:-mb-0 border-[20px] border-[#0269350D] relative rounded-[30px] overflow-hidden">
-                        <Image src={"/images/about/2.jpg"} alt="vehsf" fill className="object-cover object-top" />
+                        <Image src={"/images/about/2.jpeg"} alt="vehsf" fill className="object-cover object-top" />
                     </div>
                     <div className="w-full max-w-[470px] h-[225px] lg:h-[300px] border-[20px] border-[#0269350D] relative rounded-[30px] overflow-hidden">
-                        <Image src={"/images/about/1.jpg"} alt="vehsf" fill className="object-cover object-top" />
+                        <Image src={"/images/about/1.jpeg"} alt="vehsf" fill className="object-cover object-top" />
                     </div>
                     <div className="w-full max-w-[350px] h-[225px] lg:-ml-[20px] -mt-[20px] lg:-mt-0 border-[20px] border-[#0269350D] relative rounded-[30px] overflow-hidden">
-                        <Image src={"/images/about/3.jpg"} alt="vehsf" fill className="object-cover object-top" />
+                        <Image src={"/images/about/3.jpeg"} alt="vehsf" fill className="object-cover object-top" />
                     </div>
                 </div>
                 <div className="font-medium text-base lg:text-[20px] mt-[30px] text-center">
